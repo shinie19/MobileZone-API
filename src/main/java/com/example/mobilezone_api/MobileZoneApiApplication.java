9eb343d4bf369db3a36edea8b0f01c09798c5fdd
nguyenanhhus <nguyenanh.binoo@gmail.com>
2022-05-14 00:06:47 +0700

    Initial project

---
+++ src/main/java/com/example/mobilezone_api/MobileZoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobilezone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileZoneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileZoneApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mobilezone_api/MobileZoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilezone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileZoneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
